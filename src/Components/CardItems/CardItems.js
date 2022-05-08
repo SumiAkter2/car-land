8d3e0b264c8d128af5b1fcff757d583c3d3da14b
SumiAkter2 <akteralisha36@gmail.com>
2022-05-08 12:33:01 +0600

    added sort des

--- src/Components/CardItems/CardItems.js
+++ src/Components/CardItems/CardItems.js
@@ -3,7 +3,7 @@ import'./CardItems.css';
 
 
 const CardItems = (props) => {
-    const { name, picture, price,description } = props.item;
+    const { name, picture, price,description,quantity } = props.item;
     return (
         <div>
             <div>
@@ -15,6 +15,7 @@ const CardItems = (props) => {
                                     <h5 className="card-title">{name}</h5>
                                     <p className="card-text">{description}</p>
                                <h5>{price}</h5>
+                               <p>Quantity: {quantity}</p>
                                 </div>
                                 <div className="card-footer border-0">
                                    <button className='btn btn-primary'>Submit</button>
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -6,7 +6,7 @@ const Shop = () => {
     const [items]=UseItems();
     return (
         <div>
-            <h1>Shop</h1>
+            <h1 className='text-center my-5'>Our Collection</h1>
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3  g-4 container mx-auto">
                
                {
